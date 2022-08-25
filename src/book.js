2b6e5c0742f09b81820be5d094e240045117dcce
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-08-25 19:49:41 -0400

    Displays finished page of book.js

--- src/book.js
+++ src/book.js
@@ -12,16 +12,35 @@ return {
 function saveReview(statement, listOfReviews){
   if(listOfReviews.includes(statement) === false ){
     listOfReviews.push(statement)
+    console.log("Array of listOfReviews",listOfReviews)
   };
-  return listOfReviews
+
+console.log("Array of listOfReviews w/methods",listOfReviews);
 };
 
 
+function calculatePageCount(title) {
+return title.length * 20
+};
+
+function writeBook(bookTitle, bookCharacter, pageCount,) {
+return book = {
+title: bookTitle,
+mainCharacter: bookCharacter,
+pageCount: bookTitle.length * 20,
+genre: "fantasy"
+  };
+};
+
+function editBook(book){
+book.pageCount = book.pageCount * .75
+return book
+};
 module.exports = {
   createTitle,
    buildMainCharacter,
    saveReview,
-  // calculatePageCount,
-  // writeBook,
-  // editBook
+  calculatePageCount,
+  writeBook,
+  editBook
 };
