8bca3907135c354176c23a1b960cce0ec01db6ce
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-08-24 21:46:14 -0400

    Saving placement at reviews 3rd task

--- src/book.js
+++ src/book.js
@@ -1,11 +1,26 @@
-function createTitle() {
-  
+function createTitle(title) {
+return `The ${title}`
 }
+
+function buildMainCharacter(name,age,pronouns) {
+return {
+  name:name,
+  age:age,
+  pronouns:pronouns
+  };
+}
+function saveReview(statement,listOfReviews){
+// for(var i = 0;i < listOfReviews.length; i++) {
+//   if(listOfReviews[i] === statement){
+listOfReviews.push(statement);
+return listOfReviews
+};
 
 module.exports = {
   createTitle,
-  // buildMainCharacter,
-  // saveReview,
+   buildMainCharacter,
+
+  saveReview,
   // calculatePageCount,
   // writeBook,
   // editBook
