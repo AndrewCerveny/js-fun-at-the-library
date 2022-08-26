33238c26cd376073698effc102ca3e80797215ff
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-08-25 23:02:53 -0400

    Display solutions for first two problems of shelf.test

--- src/shelf.js
+++ src/shelf.js
@@ -1,6 +1,14 @@
+function shelfBook(bookObj,contentsOnShelves){
+if(contentsOnShelves.length < 3) {
+  contentsOnShelves.unshift(bookObj);
+  console.log("taking first book",contentsOnShelves)
+return
+  };
+};
+
 
 module.exports = {
-  // shelfBook,
+  shelfBook,
   // unshelfBook,
   // listTitles,
   // searchShelf
