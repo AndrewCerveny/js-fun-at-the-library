d344d1b4889528bedb14ec149cd830f8821c0e3c
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-08-27 13:39:40 -0400

    Finished page for shelf section

--- src/shelf.js
+++ src/shelf.js
@@ -9,12 +9,46 @@ console.log(i,sciFiShelf[i])
 return
 };
 
-function unshelfBook(sciFiShelf){
-  console.log(sciFiShelf)
+function unshelfBook(title,sciFiShelf){
+  for(var i = 0; i < sciFiShelf.length; i ++){
+    console.log(i,"current state:", sciFiShelf[i])
+    if(title === sciFiShelf[i].title){
+      sciFiShelf.splice(i,1)
+    }
+  }
+return
 };
-// trying to figure out how to access the array
-// try to unshelf object dune from name argument "dune"
-//return an arr with 2 elements left on shelf 
+
+function listTitles(fantasyShelf) {
+  for(var i = 0; i < fantasyShelf.length; i++){
+    console.log(i,"current State:",fantasyShelf[i])
+console.log (fantasyShelf.title)
+return `${fantasyShelf[0].title}, ${fantasyShelf[1].title}, ${fantasyShelf[2].title}`
+  }
+};
+
+function searchShelf(sciFiShelf, bookName) {
+for(var i = 0; i < sciFiShelf.length; i++){
+  console.log("showing Array",sciFiShelf)
+  console.log("show Dune as str",sciFiShelf[0].title)
+  if(sciFiShelf[i].title === bookName) {
+    return true
+    }
+  }   return false
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -38,6 +72,6 @@ function unshelfBook(sciFiShelf){
 module.exports = {
   shelfBook,
    unshelfBook,
-  // listTitles,
-  // searchShelf
+  listTitles,
+  searchShelf
 };
