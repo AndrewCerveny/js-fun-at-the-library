941f561b3bfaa7604a640b030bb05a97cc6571f5
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-08-28 11:29:38 -0400

    Added indentation structure to coding

--- src/book.js
+++ src/book.js
@@ -1,40 +1,39 @@
 function createTitle(title) {
-return `The ${title}`
+  return `The ${title}`
 }
 
 function buildMainCharacter(name , age, pronouns) {
-return {
-  name: name,
-  age: age,
-  pronouns: pronouns
+  return {
+    name: name,
+    age: age,
+    pronouns: pronouns
   };
-}
+};
 function saveReview(statement, listOfReviews){
   if(listOfReviews.includes(statement) === false ){
     listOfReviews.push(statement)
     console.log("Array of listOfReviews",listOfReviews)
   };
-
 console.log("Array of listOfReviews w/methods",listOfReviews);
 };
 
 
 function calculatePageCount(title) {
-return title.length * 20
+  return title.length * 20
 };
 
 function writeBook(bookTitle, bookCharacter, pageCount,) {
-return book = {
-title: bookTitle,
-mainCharacter: bookCharacter,
-pageCount: bookTitle.length * 20,
-genre: "fantasy"
+  return book = {
+    title: bookTitle,
+    mainCharacter: bookCharacter,
+    pageCount: bookTitle.length * 20,
+    genre: "fantasy"
   };
 };
 
 function editBook(book){
-book.pageCount = book.pageCount * .75
-return book
+  book.pageCount = book.pageCount * .75
+  return book
 };
 module.exports = {
   createTitle,
