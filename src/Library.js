b16d2cc1d3e82592910b4d260778d3614b87eeae
Andrew Cerveny <andrewfcerveny0628@gmail.com>
2022-08-28 20:00:56 -0400

    Add completed version of Library test

--- src/Library.js
+++ src/Library.js
@@ -29,24 +29,17 @@ return library;
  // return string
  //
 function checkoutBook(library, title, genre) {
-  console.log("Library Location",library)
-  console.log("data type", genre)
-  console.log("if genre variable willl read", library.shelves[genre])
   var booksOnShelf = library.shelves[genre]
-  var checkedOut = false
-    for (var i = 0 ; i < booksOnShelf.length; i++) {
-      console.log(i,"show books on Shelves",booksOnShelf[i])
-      if (title === booksOnShelf[i].title) {
-      console.log("taking of book from whereEver it is", booksOnShelf.splice(i,1))
-       booksOnShelf.splice(i,1);
-       checkedOut = true;
-      }
-return
-  checkedOut ? `you have now checked out ${title} from the ${library.name}`
-  : `Sorry, there are currently no copies of ${title} available at the ${library.name}`
+  for (var i = 0 ; i <= booksOnShelf.length; i++) {
+    if (booksOnShelf.length && title === booksOnShelf[i].title) {
+      booksOnShelf.splice(i, 1);
+      return  `You have now checked out ${title} from the ${library.name}`
+    } else {
+      return `Sorry, there are currently no copies of ${title} available at the ${library.name}`
     }
-  };
-// ternary operator else if shorthand is ?:
+  }
+};
+// ternary operator  if-else shorthand is ?:
 // could not get last test to work.. don't know the issue. could not get fiction shelf to accept if conditional and clear. Expected empty[] instead library.fiction :[Pride and prideAndPrejudice_Object]
 
 // // for(i = 0; i < library.shelves[genre].length; i++){
